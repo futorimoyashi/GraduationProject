c4e960e0b6621638178164218ceaf84829b498f4
futorimoyashi <nvtt1906@gmail.com>
2020-09-12 20:24:31 +0700

    fix UserController

--- src/main/java/com/fpoly/controllers/UserController.java
+++ src/main/java/com/fpoly/controllers/UserController.java
@@ -1,13 +1,6 @@
 package com.fpoly.controllers;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fpoly.models.UserApplication;
 import com.fpoly.services.UserService;
@@ -15,22 +8,15 @@ import com.fpoly.services.UserService;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	
-	@Autowired
+
 	private UserService userService;
-	
-	@PostMapping("/add")
-	public void add(UserApplication user) {
-		userService.save(user);
+
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
-	
-//	@GetMapping("/")
-//	public List<UserApplication> selectAll() {
-//		List<UserApplication> users = (List<UserApplication>) userService.findAll();
-//		return users;
-//	}
-	@GetMapping("/")
-	public String select() {
-		return "Graduation Project";
+
+	@PostMapping("/add")
+	public void add(@RequestBody UserApplication user) {
+		userService.save(user);
 	}
 }
