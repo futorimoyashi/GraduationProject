30f90eee0f07955efca770de5b0122eeb28533c4
namtruong244 <n.truongtp@gmail.com>
2020-09-13 10:52:20 +0700

    Init Repo, Service for all model

--- src/main/java/com/fpoly/services/CollectionServiceImpl.java
+++ src/main/java/com/fpoly/services/CollectionServiceImpl.java
@@ -15,46 +15,57 @@ public class CollectionServiceImpl implements CollectionService{
         this.collectionRepository = collectionRepository;
     }
 
-    public <S extends Collection> S save(S s) {
-        return collectionRepository.save(s);
+    @Override
+    public Collection save(Collection entity) {
+        return collectionRepository.save(entity);
     }
 
-    public <S extends Collection> Iterable<S> saveAll(Iterable<S> iterable) {
+    @Override
+    public Iterable<Collection> saveAll(Iterable<Collection> iterable) {
         return collectionRepository.saveAll(iterable);
     }
 
+    @Override
     public Optional<Collection> findById(Integer integer) {
         return collectionRepository.findById(integer);
     }
 
+    @Override
     public boolean existsById(Integer integer) {
         return collectionRepository.existsById(integer);
     }
 
+    @Override
     public Iterable<Collection> findAll() {
         return collectionRepository.findAll();
     }
 
+    @Override
     public Iterable<Collection> findAllById(Iterable<Integer> iterable) {
         return collectionRepository.findAllById(iterable);
     }
 
+    @Override
     public long count() {
         return collectionRepository.count();
     }
 
+    @Override
     public void deleteById(Integer integer) {
         collectionRepository.deleteById(integer);
     }
 
+    @Override
     public void delete(Collection collection) {
         collectionRepository.delete(collection);
     }
 
+    @Override
     public void deleteAll(Iterable<? extends Collection> iterable) {
         collectionRepository.deleteAll(iterable);
     }
 
+    @Override
     public void deleteAll() {
         collectionRepository.deleteAll();
     }
